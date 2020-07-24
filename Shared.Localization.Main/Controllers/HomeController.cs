1bbbbe02e2b0fd55783829b1db6a4f5ed356e183
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2020-07-24 17:08:09 +0300

    Added empty view that just shows a string from localization resources.

---
+++ Shared.Localization.Main/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿namespace Shared.Localization.Main.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Localization;
+
+    public class HomeController : Controller
+    {
+        private readonly IStringLocalizer<HomeController> localizer;
+
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            this.localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            var allResources = this.localizer.GetAllStrings();
+
+            this.ViewBag.Test = this.localizer["Test"];
+            return this.View();
+        }
+    }
+}
